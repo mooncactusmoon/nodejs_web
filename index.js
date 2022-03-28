60bdb01a5c51039f7c7ad1bb4bcab36695644d36
Moon <mooncactusmoon@gmail.com>
2022-03-28 12:38:53 +0800

    server模組化 + 使用index去開啟

---
+++ index.js
@@ -0,0 +1,5 @@
+//從'server'模組中匯入server物件
+let server = require('./server');
+
+//啟動伺服器
+server.start();
--- server.js
+++ server.js
@@ -1,10 +1,39 @@
+//模組化
+//匯出'server'物件，物件包含了start函數
+//物件格式為
+/*
+{
+    start
+}
+*/
+//這個物件匯入其他檔案中即可使用。可用任意名字來接收這物件
+const http= require("http");
+const start= () =>{
+    const onRequest=(request,response)=>{
+        console.log("Request received. :>");
+        response.writeHead(200,{"Content-Type":"text/plain;charset=utf-8"});
+        response.write("Hello World 2022/03/28 :3 !");
+        response.end();
+    }
+    //把函數當作參數傳遞
+    http.createServer(onRequest).listen(8888);
+
+    console.log("Server has started...");
+    console.log("在瀏覽器中開啟localhost:8888吧");
+}
+
+
+exports.start=start;
+
+
+/* 原本寫法(非模組化)
 const http= require("http");
 
 http.createServer(function(request,response){
     response.writeHead(200,{"Content-Type":"text/plain"});
     response.write("Hello World 2022/03/28 :>");
     response.end();
-}).listen(8888);
+}).listen(8888); */
 
 //亦可以用箭頭函數
 /*
